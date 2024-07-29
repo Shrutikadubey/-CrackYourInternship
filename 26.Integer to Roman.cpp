f5446c5b55b60af45f941aec509f30b5b288643c
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2024-07-29 23:06:26 +0530

    Create 26.Integer to Roman.cpp

---
+++ 26.Integer to Roman.cpp
@@ -0,0 +1,12 @@
+//problem:Seven different symbols represent Roman numerals with the following values:
+
+//solution:
+vector<pair<int, string>> values = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+        string roman;
+        for (auto& value : values) {
+            while (num >= value.first) {
+                roman += value.second;
+                num -= value.first;
+            }
+        }
+        return roman;
