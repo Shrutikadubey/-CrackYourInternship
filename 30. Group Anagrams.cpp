82e637dfdf3745a32ee39cbcf3d6372e8853c176
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2024-07-31 23:11:41 +0530

    Create 30. Group Anagrams.cpp

---
+++ 30. Group Anagrams.cpp
@@ -0,0 +1,15 @@
+//problem:Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+//solution:
+unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto& p : mp) {
+            result.push_back(p.second);
+        }
+        return result;
