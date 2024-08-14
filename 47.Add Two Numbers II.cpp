1867f4db79ef657a9402cc873828bf09e08f61e5
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2024-08-14 12:38:27 +0530

    Create 47.Add Two Numbers II.cpp

---
+++ 47.Add Two Numbers II.cpp
@@ -0,0 +1,20 @@
+//problem:You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+//You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+//solution: ListNode dummy(0);
+        ListNode* curr = &dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+        }
+
+
+
+        return dummy.next;
